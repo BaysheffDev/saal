17f2a0894f1d26e24a0ade1c4455120f8055773d
Alex Saliu <alex.saliu@gmail.com>
2019-09-12 20:25:47 +0930

    dataload ready for testing

--- dataload.js
+++ dataload.js
@@ -17,15 +17,23 @@ const insertMeeting = (name, date) => {
 
 const insertEvent = (meetingId, name) => {
     const insertQuery = {
-      text: `INSERT INTO events(meeting_id, name) VALUES($1, $2)`,
+      text: `INSERT INTO events(meeting_id, name) VALUES($1, $2) RETURNING id`,
       values: [meetingId, name],
     }
     return insertQuery;
 }
 
+const checkAthlete = (name) => {
+  const selectQuery = {
+    text: `SELECT * FROM athletes WHERE name = $1`,
+    values: [name],
+  }
+  return selectQuery;
+}
+
 const insertAthlete = (name) => {
     const insertQuery = {
-      text: `INSERT INTO events(name) VALUES($1)`,
+      text: `INSERT INTO events(name) VALUES($1) RETURNING id`,
       values: [name],
     }
     return insertQuery;
@@ -33,7 +41,7 @@ const insertAthlete = (name) => {
 
 const insertResult = (eventId, athleteId, mark, time, position, round) => {
     const insertQuery = {
-      text: `INSERT INTO events(event_id, athlete_id, mark, time, position, round) VALUES($1, $2, $3, $4, $5, $6)`,
+      text: `INSERT INTO events(event_id, athlete_id, mark, time, position, round) VALUES($1, $2, $3, $4, $5, $6) RETURNING *`,
       values: [eventId, athleteId, mark, time, position, round],
     }
     return insertQuery;
@@ -41,13 +49,61 @@ const insertResult = (eventId, athleteId, mark, time, position, round) => {
 
 async function loadData() {
     const meetings = await saal.getMeetings();
-    for (let i = 0, len = meetings.length; i < len; i++) {
+    // Insert meetings into meetings table
+    for (let i = 0, len = meetings.length; i < 1; i++) {
         try {
-            const res = await db.query(insertMeeting(meetings[i].name, meetings[i].date))
-            console.log("HEHEHEHE: ", res.rows[0]);
+            const newMeeting = await db.query(insertMeeting(meetings[i].name, meetings[i].date))
+            console.log("New meeting: ", newMeeting);
+            const events = await saal.getMeetingRaces(meetings[i].link);
+            // Insert events for this meeting into events table with meeting id as FK
+            for (let j = 0, len = events.length; j < len; j++) {
+              try {
+                const meetingId = newMeeting.rows[0].id;
+                const newEvent = await db.query(insertEvent(events[j].name, meetingId));
+                console.log("New Event: ", newEvent);
+                const eventId = newEvent.rows[0].id;
+                const results = await saal.getRaceReults(events[j].link);
+                // Insert results for each event into results table with event id as FK
+                for (let k = 0, len = results.length; k < len; k++) {
+                  // Check if athlete exists in athletes table. If no, add them.
+                  // Set the athlete id
+                  const athleteId = 0;
+                  try {
+                    const athleteCheck = db.query(checkAthlete(results[k].name));
+                    if (!athleteCheck.rows[0].id) {
+                      const newAthlete = db.query(insertAthlete(results[k].name));
+                      athleteId = newAthlete.rows[0].id;
+                      console.log("New Athlete: ", athleteId);
+                    }
+                    else {
+                      athleteId = athleteCheck.rows[0].id;
+                      console.log("Athlete Exists: ", athleteId);
+                    }
+                  }
+                  catch(err) {
+                    console.log("ERROR AT ATHLETE LEVEL ", err)
+                  }
+                  // Insert result
+                  const mark = results[k].mark;
+                  const time = results[k].time;
+                  const position = results[k].position;
+                  const round = results[k].round;
+                  try {
+                    const newResult = db.query(insertResult(eventId, athleteId, mark, time, position, round));
+                    console.log("New Result: ", newResult);
+                  }
+                  catch (err) {
+                    console.log("ERROR AT RESULT LEVEL ", err)
+                  }
+                }
+              }
+              catch(err) {
+                console.log("ERROR AT EVENT LEVEL ", err)
+              }
+            }
         }
-        catch (err) {
-            console.log("COULDN'T RUN QUERY ", err)
+        catch(err) {
+            console.log("ERROR AT MEETING LEVEL ", err);
         }
     }
 }
