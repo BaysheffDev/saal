622137166b6da492f6e5ae78c9c2699fd2498eb2
Alex Saliu <alex.saliu@gmail.com>
2019-09-13 09:22:54 +0930

    tidying up data load functions

--- dataload.js
+++ dataload.js
@@ -46,6 +46,35 @@ const insertResult = (eventId, athleteId, mark, time, position, round) => {
     }
     return insertQuery;
 }
+
+// Check if athlete exists in athletes table. If no, add them.
+// Return athlete id
+async function athleteQuery(name) {
+  let id = 0;
+  try {
+    const athleteCheck = await db.query(checkAthlete(name));
+    console.log("ATHLETE CHECK: ", athleteCheck.rows);
+    if (athleteCheck.rows.length < 1) {
+        try {
+            const newAthlete = await db.query(insertAthlete(name));
+            id = newAthlete.rows[0].id;
+            console.log("New Athlete: ", athleteId);
+        }
+        catch(err) {
+            console.log("ERROR AT ATHLETE INSERT LEVEL ", err)
+        }
+    }
+    else {
+        id = athleteCheck.rows[0].id;
+        console.log("Athlete Exists: ", athleteId);
+    }
+  }
+  catch(err) {
+    console.log("ERROR AT ATHLETE CHECK LEVEL ", err)
+  }
+  return id;
+}
+
 // 51 meetings should be 3 seasons back
 async function loadData() {
     const meetings = await saal.getMeetings();
@@ -53,51 +82,23 @@ async function loadData() {
     for (let i = 0; i < 51; i++) {
         try {
             const newMeeting = await db.query(insertMeeting(meetings[i].name, meetings[i].date))
-            console.log("New meeting: ", newMeeting);
             const events = await saal.getMeetingRaces(meetings[i].link);
             // Insert events for this meeting into events table with meeting id as FK
             for (let j = 0, len = events.length; j < len; j++) {
               try {
                 const meetingId = newMeeting.rows[0].id;
                 const newEvent = await db.query(insertEvent(meetingId, events[j].name));
-                console.log("New Event: ", newEvent);
                 const eventId = newEvent.rows[0].id;
                 const results = await saal.getRaceResults(events[j].link);
                 // Insert results for each event into results table with event id as FK
                 for (let k = 0, len = results.length; k < len; k++) {
-                  // Check if athlete exists in athletes table. If no, add them.
-                  // Set the athlete id
-                  let athleteId = 0;
-                  try {
-                    const athleteCheck = await db.query(checkAthlete(results[k].name));
-                    console.log("ATHLETE CHECK: ", athleteCheck);
-                    console.log("===================");
-                    if (athleteCheck.rows.length < 1) {
-                        try {
-                            const newAthlete = await db.query(insertAthlete(results[k].name));
-                            athleteId = newAthlete.rows[0].id;
-                            console.log("New Athlete: ", athleteId);
-                        }
-                        catch(err) {
-                            console.log("ERROR AT ATHLETE INSERT LEVEL ", err)
-                        }
-                    }
-                    else {
-                        athleteId = athleteCheck.rows[0].id;
-                        console.log("Athlete Exists: ", athleteId);
-                    }
-                  }
-                  catch(err) {
-                    console.log("ERROR AT ATHLETE CHECK LEVEL ", err)
-                  }
+                  // Get athlete id for the new or existing athlete
+                  let athleteId = await athleteQuery(results[k].name);
                   // Insert result
-                  const mark = results[k].mark;
-                  const time = results[k].time;
-                  const position = results[k].position;
-                  const round = results[k].round;
                   try {
-                    const newResult = await db.query(insertResult(eventId, athleteId, mark, time, position, round));
-                    console.log("New Result: ", newResult);
+                    const newResult = await db.query(
+                      insertResult(eventId, athleteId, results[k].mark, results[k].time, results[k].position, results[k].round);
+                    );
                   }
                   catch (err) {
                     console.log("ERROR AT RESULT LEVEL ", err)
